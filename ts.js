ba80b1b75c2bf7d11ddc7c3bfe4c80fbca17fb57
Severnyi-Lis <xnestro@mail.ru>
2024-01-30 07:22:33 +0300

    first commit

---
+++ ts.js
@@ -0,0 +1,28 @@
+window.addEventListener(
+    'load',
+    kuk
+);
+
+function kuk(){
+    ttt.addEventListener(
+    'click',
+    nnn
+    )
+}
+
+function nnn(){
+   var p = document.forms.trigger.prpr.value
+    if(p.length==''){
+        window.alert("Не указано Ваше Имя");
+    }else{} 
+    var z =document.forms.trigger.prpr_2.value
+    if(z.length==''){
+        window.alert("Не указано Ваша Фамилия");
+    }else{}
+    var f = document.forms.trigger.prpr_3.value
+    if(f.length==''){
+        window.alert("Не указано Ваше Отчество"); 
+    }else{}
+}  
+
+
